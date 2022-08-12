0d7a2f6bb998091609061317e3087743bc637907
vivianchan1998 <b05901179@ntu.edu.tw>
2022-08-12 17:07:48 -0400

    init

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
 import cv2
-print("HI")
+#import cv2.aruco as aruco
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+
+camera = PiCamera()
+
+while(True):
+
+    rawCapture = PiRGBArray(camera)
+
+    camera.capture(rawCapture, format="bgr")
+    frame = rawCapture.array
+  
+    cv2.imshow('frame', frame)
+    
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
+    arucoParams = cv2.aruco.DetectorParameters_create()
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+vid.release()
+cv2.destroyAllWindows()
