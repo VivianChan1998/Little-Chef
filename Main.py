e972a306b4035acd49d0a3b7130efd04aa14c957
VivianChan1998 <b05901179@ntu.edu.tw>
2022-08-16 22:55:35 -0400

    printing tiles

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-from argparse import Action
 import numpy as np
+import emoji
 from Enums import Stations
 from Enums import Actions
 import Food
@@ -11,13 +11,24 @@ Global Parameters Init
 board = np.full((7,6), Stations.NONE)
 board[6,3] = Stations.FINISH # make sure the location of finish counter
 user = User()
-counter = []
+tiles = np.full(100, Actions.NONE) # RPi code should break it down into one single array for this code
+tile_idx = -1
+finished = []
 
 '''
 TEMP
 '''
 board[5][4] = Stations.STOVE 
 board[2][3] = Stations.MEAT
+tiles[0] = Actions.LEFT
+tiles[1] = Actions.DOWN
+tiles[2] = Actions.UP
+tiles[3] = Actions.RIGHT
+tiles[4] = Actions.COOK
+tiles[5] = Actions.CHOP
+tiles[6] = Actions.PUT
+tiles[7] = Actions.TAKE
+
 ''''''
 
 def print_board():
@@ -38,11 +49,49 @@ def print_board():
             print(' ' + '   ', end = '' if j!=5 else '\n')
         else:
             print('|' + tmp + '|', end = '')
-
     print('_________________________\n')
 
+def print_actions():
+    for i in range(len(tiles)):
+        if tiles[i] == Actions.NONE:
+            print()
+            break
+        if i == tile_idx:
+            print(emoji.emojize(":backhand_index_pointing_down: "), end = '')
+        else:
+            print("  ", end = '')
+    for i in range(len(tiles)):
+        if tiles[i] == Actions.NONE:
+            break
+        tile = tiles[i]
+        if tile == Actions.UP:
+            print(emoji.emojize(":up_arrow:"), end = '')
+        elif tile == Actions.DOWN:
+            print(emoji.emojize(":down_arrow:"), end = '')
+        elif tile == Actions.LEFT:
+            print(emoji.emojize(":left_arrow:"), end = '')
+        elif tile == Actions.RIGHT:
+            print(emoji.emojize(":right_arrow:"), end = '')
+        elif tile == Actions.COOK:
+            print(emoji.emojize(":cooking:"), end = '')
+        elif tile == Actions.CHOP:
+            print(emoji.emojize(":kitchen_knife:"), end = '')
+        elif tile == Actions.PUT:
+            print(emoji.emojize(":palm_down_hand:"), end = '')
+        elif tile == Actions.TAKE:
+            print(emoji.emojize(":palm_up_hand:"), end = '')
+        print(' ', end='')
 
+
+
+def wait_next():
+    key = input("press enter to continue...")
+    # RPi-todo: return true when motor stuff done
+    return key
 
 
 if __name__ == "__main__":
-    print_board()
+    while True:
+        if wait_next() == '':
+            print_actions()
+            print_board()
